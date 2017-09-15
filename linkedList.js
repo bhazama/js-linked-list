aff91fd3b1d03759a8e01576dbd2eff4334d3efe
Brendan Hazama <brendanhazama@Brendans-MacBook-Pro.local>
2017-09-14 20:05:33 -1000

    finished up to remove. working on insert

--- linkedList.js
+++ linkedList.js
@@ -7,8 +7,9 @@ function linkedListGenerator(){
   var list = null;
   var head = null;
   var tail = null;
-  var previous;
-  var current;
+  var previousNode;
+  var currentNode;
+  var nextNode;
   var length = 0;
   var temp = {};
 
@@ -45,46 +46,72 @@ function linkedListGenerator(){
       value : val,
       next : null
     };
-    if (list === null){
-      list = node;
-      tail = node;
-      head = node;
-      //current = node;
-    }else {
-      tail = node;
-      tail.next = node;
-      //previous = node;
-      //current = node;
-    }
-    length++;
-    return tail;
 
-}
+    if(head === null && tail === null){ //if list is empty
+      head = node;                      // add node object, change pointer of "head" and "tail" to the new object
+      tail = node;
+    }else{                              // if list isnt empty
+      tail.next = node;                 // add next node object to the "next" value in the tail
+      tail = node;                      // change the tail pointer to the new node object added
+    }
+    length++;                           // iterate the length of the list by the new object
+    return node;                        // returns the newly added node object
+  }
 
   function get(number){
-    temp = head;
-    for(var i = 0; i < number; i++){
-      if(temp.next === null){
-        return false;
-      }else if (temp.length === number) {
-        return temp;
-      }{
-
+    current = head;
+    if(number < length){
+      for(var i = 0; i < number; i++){
+        current = current.next;
       }
-
+      return current;
     }
-
+    return false;
   }
 
   function remove(number){
+    var removeNode = get(number);
+    var previousNode = get(number - 1);
 
-  }
+    if (removeNode === false) {
+      return false;
+
+    } else if (number === 0) {
+      head = head.next;
+
+    } else if (removeNode === tail) {
+      tail = previousNode;
+      tail.next = false;
+
+    } else {
+      previousNode.next = removeNode.next;
+    }
+
+
+    }
 
   function insert(value, number){
-
+    previousNode = get(number - 1);
+    currentNode = get(number);
+    var node = {
+      val : value,
+      next : currentNode
+    };
+    if(number === 0) {
+      head = node;
+      node.next = currentNode;
+    }else if (number < 0){
+      return false;
+    }else if(currentNode === false){
+      return false;
+    }else {
+      previousNode.next = node;
+      node.next = currentNode;
+    }
   }
 
 
 }
 
-console.log(linkedListGenerator().get(5));
+
+console.log(linkedListGenerator().remove(3));

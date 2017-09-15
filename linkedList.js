43e651b4e1bfcc0a38cf133a65642f0c90b6f21e
Brendan Hazama <brendanhazama@Brendans-MacBook-Pro.local>
2017-09-14 17:01:31 -1000

    working on get method

--- linkedList.js
+++ linkedList.js
@@ -4,5 +4,87 @@
  * @return {Object} an object exposing methods to be used to manipulate a linked list
  */
 function linkedListGenerator(){
+  var list = null;
+  var head = null;
+  var tail = null;
+  var previous;
+  var current;
+  var length = 0;
+  var temp = {};
+
+
+
+  return {
+    getHead : getHead,
+    getTail : getTail,
+    add : add,
+    get : get,
+    remove : remove,
+    insert : insert,
+    makeNode : makeNode
+
+  };
+
+  function makeNode(data){
+    var node = {};
+    node.value = data;
+    node.next = null;
+    return node;
+  }
+
+  function getHead(){
+    return head;
+  }
+
+  function getTail(){
+    return tail;
+  }
+
+  function add(val){
+    var node = {
+      value : val,
+      next : null
+    };
+    if (list === null){
+      list = node;
+      tail = node;
+      head = node;
+      //current = node;
+    }else {
+      tail = node;
+      tail.next = node;
+      //previous = node;
+      //current = node;
+    }
+    length++;
+    return tail;
 
 }
+
+  function get(number){
+    temp = head;
+    for(var i = 0; i < number; i++){
+      if(temp.next === null){
+        return false;
+      }else if (temp.length === number) {
+        return temp;
+      }{
+
+      }
+
+    }
+
+  }
+
+  function remove(number){
+
+  }
+
+  function insert(value, number){
+
+  }
+
+
+}
+
+console.log(linkedListGenerator().get(5));
